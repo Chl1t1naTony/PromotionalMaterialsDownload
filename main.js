28cbcbd9dff7a1664ba0c9517be2e30d578d96dc
Chl1t1naTony <127829175+Chl1t1naTony@users.noreply.github.com>
2023-12-14 11:20:16 +0800

    fix full url

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let SHEET_ID = '1M2ZLgwyBQnva7nrKadQsLs8C-Sw-ZaKtoXrgGk5o7mQ';
-let SHEET_TITLE = 'MaterialList';
-let SHEET_RANGE = 'A:D';
+	let SHEET_ID = '1M2ZLgwyBQnva7nrKadQsLs8C-Sw-ZaKtoXrgGk5o7mQ';
+	let SHEET_TITLE = 'MaterialList';
+	let SHEET_RANGE = 'A:D';
 
-let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_TITLE);
-
-fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-	console.log(rep)
-})
+	let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
+	console.log(FULL_URL);
+	fetch(FULL_URL)
+	.then(res => res.text())
+	.then(rep => {
+		console.log(rep)
+	})
